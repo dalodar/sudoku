23aa7e4cd0985527baf8c3c64da68c858c3ca532
pierremm <46106898+pierremm@users.noreply.github.com>
2019-03-19 15:33:54 +0100

    Création du fichier
    Welcome on board !

---
+++ sudoku.js
@@ -0,0 +1,24 @@
+// SUDIKU
+
+// Fonction 1
+// parse (maligne, url); <- Fichier
+
+
+// Fonction 2
+// load (string, param);
+
+
+
+// Fonction 3
+// grid.update (row, col, value)
+
+
+// Fonction 4
+// grid to String (grid)
+// askValue();
+//
+
+
+
+
+
